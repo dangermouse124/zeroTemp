d4a02e2852cdd17ba661736a99cdbc8db406e4a3
dangermouse124 <pimail124@gmail.com>
2019-12-14 12:29:30 +0800

    temp test added

--- temperature/views.py
+++ temperature/views.py
@@ -4,3 +4,16 @@ def home(request):
 
     return render(request, 'temperature/home.html')
 
+def getTemp(request):
+
+    temp_sensor = '/sys/bus/w1/devices/28-0314640d89ff/w1_slave'
+
+    tempStore = open(temp_sensor, 'r')	
+    data = tempStore.read()
+    tempStore.close()
+    tempData = data.split("\n")[1].split(" ")[9]
+    temperature = float(tempData[2:])
+    temperature = temperature/1000
+    print(temperature)
+
+    return render(request, 'temperature/home.html')
